fea2d061f104cb8db369d19de9625b17401f72fe
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-05-09 22:13:14 -0400

    Initial working test (somewhat)

--- app/src/androidTest/java/com/vandenbreemen/sim_assistant/MainScreenTest.kt
+++ app/src/androidTest/java/com/vandenbreemen/sim_assistant/MainScreenTest.kt
@@ -1,6 +1,8 @@
 package com.vandenbreemen.sim_assistant
 
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,8 +15,13 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainScreenTest {
 
+    @get:Rule
+    val activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
     @Test
     fun shouldBeAbleToStart(){
+
+        activityRule.launchActivity(null)
 
     }
 
