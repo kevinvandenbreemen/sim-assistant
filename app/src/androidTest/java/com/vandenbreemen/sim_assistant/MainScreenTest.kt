1c6ad341a801ddcf7bb0b31fbb56ed91d864daef
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-05-09 21:53:34 -0400

    Start work on the ultimate simming app

---
+++ app/src/androidTest/java/com/vandenbreemen/sim_assistant/MainScreenTest.kt
@@ -0,0 +1,21 @@
+package com.vandenbreemen.sim_assistant
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * <h2>Intro</h2>
+ *
+ * <h2>Other Details</h2>
+ * @author kevin
+ */
+@RunWith(AndroidJUnit4::class)
+class MainScreenTest {
+
+    @Test
+    fun shouldBeAbleToStart(){
+
+    }
+
+}
