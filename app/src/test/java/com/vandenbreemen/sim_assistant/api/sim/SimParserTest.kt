eaafc83c86faa921ab0b22670011794d69deedf6
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-05-27 13:34:36 -0400

    Additional testing for non-covered scenarios

--- app/src/test/java/com/vandenbreemen/sim_assistant/api/sim/SimParserTest.kt
+++ app/src/test/java/com/vandenbreemen/sim_assistant/api/sim/SimParserTest.kt
@@ -76,6 +76,42 @@ class SimParserTest {
     }
 
     @Test
+    fun shouldHandleMultilineSentence(){
+        //  Arrange
+        val sim = Sim(
+                "Test Sim", "Kevin", System.currentTimeMillis(),
+                "((Corridor - USS Imaginary))\n\nIt was a\n bleak, dark and\nstormy night.  Bill\nhad just arrived.  His rage was blinding."
+        )
+
+        //  Act
+        val parser = SimParser(sim)
+        val utterances = parser.toUtterances()
+
+        //  Assert
+        println(utterances)
+        assertEquals("Number of Utterances", 6, utterances.size)
+        assertEquals("Split sentence", "It was a bleak, dark and stormy night", utterances[3])
+    }
+
+    @Test
+    fun shouldHandleIncompleteSentence(){
+        //  Arrange
+        val sim = Sim(
+                "Test Sim", "Kevin", System.currentTimeMillis(),
+                "((Corridor - USS Imaginary))\n\nIt was a\n bleak, dark and\nstormy night"
+        )
+
+        //  Act
+        val parser = SimParser(sim)
+        val utterances = parser.toUtterances()
+
+        //  Assert
+        println(utterances)
+        assertEquals("Number of Utterances", 4, utterances.size)
+        assertEquals("Split sentence", "It was a bleak, dark and stormy night", utterances[3])
+    }
+
+    @Test
     fun shouldCountUtterancesCorrectly(){
         val sim = Sim(
                 "Test Sim",
