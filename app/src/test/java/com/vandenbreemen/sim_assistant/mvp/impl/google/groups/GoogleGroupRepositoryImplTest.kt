b7ebeb306f9b96059010b16bd36dc596abb14534
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-06-24 09:29:27 -0400

    Prevent accidentally adding the same group twice

--- app/src/test/java/com/vandenbreemen/sim_assistant/mvp/impl/google/groups/GoogleGroupRepositoryImplTest.kt
+++ app/src/test/java/com/vandenbreemen/sim_assistant/mvp/impl/google/groups/GoogleGroupRepositoryImplTest.kt
@@ -3,6 +3,7 @@ package com.vandenbreemen.sim_assistant.mvp.impl.google.groups
 import com.vandenbreemen.sim_assistant.app.SimAssistantApp
 import com.vandenbreemen.sim_assistant.data.GoogleGroup
 import com.vandenbreemen.sim_assistant.mvp.google.groups.GoogleGroupRepository
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
 import org.junit.Before
 import org.junit.Test
@@ -28,6 +29,16 @@ class GoogleGroupRepositoryImplTest {
         googleGroupRepository.addGoogleGroup(googleGroup)
 
         assertFalse("Groups", googleGroupRepository.getGoogleGroups().isEmpty())
+    }
+
+    @Test
+    fun shouldNotAllowStoringSameGroupName(){
+        var googleGroup = GoogleGroup(0L, "kevin-group")
+        googleGroupRepository.addGoogleGroup(googleGroup)
+        googleGroup = GoogleGroup(0L, "kevin-group")
+        googleGroupRepository.addGoogleGroup(googleGroup)
+
+        assertEquals("Unique Name", 1, googleGroupRepository.getGoogleGroups().size)
     }
 
 }
