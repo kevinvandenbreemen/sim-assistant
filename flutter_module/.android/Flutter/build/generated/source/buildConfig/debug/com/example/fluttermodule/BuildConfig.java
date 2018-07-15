2f36c430d936527bc3bc8ffef3fe59d0fdd76213
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-07-15 09:22:55 -0400

    Include Android/IOS folders

---
+++ flutter_module/.android/Flutter/build/generated/source/buildConfig/debug/com/example/fluttermodule/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.fluttermodule;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.fluttermodule";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
